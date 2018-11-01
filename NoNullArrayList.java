611535c8fb91a09d4b17f033afc6abf997804492
Elias Ferguson <eferguson00@stuy.edu>
2018-11-01 11:47:10 -0400

    created class skeleton

---
+++ NoNullArrayList.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+public class NoNullArrayList<T> extends ArrayList<T> {
+  public NoNullArrayList() {
+    super();
+  }
+  public NoNullArrayList(int startingCapacity) {
+    super(startingCapacity);
+  }
+  public T set(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException();
+    }
+    else {
+      return super.set(index, element);
+    }
+  }
+}
