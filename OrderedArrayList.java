bd6fe78680650d3c9200a0f6ec10f1ad7b6c8e1c
Elias Ferguson <eferguson00@stuy.edu>
2018-11-02 11:35:11 -0400

    added prelim sort helper and set method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,5 +5,20 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int startingCapacity) {
     super(startingCapacity);
   }
-
+  public void sorter() {
+    for (int i = 0; i < this.size() - 1; i++) {
+      int val = this.get(i).compareTo(this.get(i + 1));
+      if (val > 0) {
+        T first = this.get(i);
+        T second = this.get(i + 1);
+        this.set(i, second);
+        this.set(i + 1, first);
+      }
+    }
+  }
+  public T set(int index, T element) {
+    T rVal = super.remove(index);
+    add(element);
+    return rVal;
+  }
 }
