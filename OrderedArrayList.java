fa4d8d8d5fd7f248617651d6640fe187e4624859
Elias Ferguson <eferguson00@stuy.edu>
2018-11-02 11:45:00 -0400

    deleted sorter method, trying to create a find index method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,22 +5,19 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int startingCapacity) {
     super(startingCapacity);
   }
-  public void sorter() {
-    for (int i = 0; i < this.size() - 1; i++) {
+  public int findIndex(T element) {
+    for (int i = 0; i < this.size(); i++) {
       int val = this.get(i).compareTo(this.get(i + 1));
       if (val > 0) {
-        T first = this.get(i);
-        T second = this.get(i + 1);
-        this.set(i, second);
-        this.set(i + 1, first);
+
       }
     }
   }
   public boolean add(T element) {
+    int index = findIndex(element);
     super.add(element);
-    this.sort();
   }
-  
+//  public void add
   public T set(int index, T element) {
     T rVal = super.remove(index);
     add(element);
