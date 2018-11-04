32b5c6cc30a93c0f09106dfb98077e125c3c19f2
Elias Ferguson <eferguson00@stuy.edu>
2018-11-04 16:16:15 -0500

    finished findIndex prelim and set prelim

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -6,21 +6,26 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super(startingCapacity);
   }
   public int findIndex(T element) {
+    int returner = 0;
     for (int i = 0; i < this.size(); i++) {
       int val = this.get(i).compareTo(this.get(i + 1));
-      if (val > 0) {
-
+      if (val <= 0) {
+        returner = i;
       }
     }
+    return returner;
   }
   public boolean add(T element) {
     int index = findIndex(element);
-    super.add(element);
+    super.add(index, element);
+    return true;
   }
-//  public void add
+  public void add(int index, T element) {
+    index = findIndex(element);
+    super.add(index, element);
+  }
   public T set(int index, T element) {
-    T rVal = super.remove(index);
-    add(element);
-    return rVal;
+    index = findIndex(element);
+    return super.set(index, element);
   }
 }
